074469a2874376ae41475a2551d3d1ef4db2db32
tinkerbug <114777377+tinkerbug@users.noreply.github.com>
2026-01-09 08:24:19 -0500

    Update configuration for AP and GNSS pins

--- TinkerRTKLoRa/TinkerRTK_LoRa_Base/config.h
+++ TinkerRTKLoRa/TinkerRTK_LoRa_Base/config.h
@@ -4,21 +4,21 @@
 struct config 
 {
     // Access point name and password
-    inline static constexpr const char* AP_SSID = "tinkerbug_rover";
+    inline static constexpr const char* AP_SSID = "tinkerbug_base";
     inline static constexpr const char* AP_PASS = "Tinkerbug";
 
     // NMEA from SkyTraq
-    static constexpr uint8_t GNSS_TX = 35;
-    static constexpr uint8_t GNSS_RX = 36;
+    static constexpr uint8_t GNSS_TX = 47;
+    static constexpr uint8_t GNSS_RX = 48;
 
-    //  LoRa Radio Pins (SX1262)
-    static constexpr uint8_t  L_DIO1 = 44;
-    static constexpr uint8_t  L_CS   = 7;
-    static constexpr uint8_t  L_BUSY = 43;
-    static constexpr uint8_t  L_RST  = 6;
-    static constexpr uint8_t  L_MISO = 2;
-    static constexpr uint8_t  L_MOSI = 4;
-    static constexpr uint8_t  L_SCK  = 5;
+    // LoRa Radio (LLC68)
+    static constexpr uint8_t  L_DIO1 = 18;
+    static constexpr uint8_t  L_CS   = 38;
+    static constexpr uint8_t  L_BUSY = 17;
+    static constexpr uint8_t  L_RST  = 37;
+    static constexpr uint8_t  L_MISO = 34;
+    static constexpr uint8_t  L_MOSI = 35;
+    static constexpr uint8_t  L_SCK  = 36;
 
     // Neopix LED Driver Pin
     static constexpr int NEO_PIN = 26;
