669ab769559f827305e0b37d4075fdd38b0b4d06
tinkerbug <114777377+tinkerbug@users.noreply.github.com>
2026-01-09 08:27:34 -0500

    Update copyright year and add battery cell handling

--- TR_RTKRoverWebUI/TR_RTKRoverWebUI.h
+++ TR_RTKRoverWebUI/TR_RTKRoverWebUI.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Tinkerbug Robotics
+// Copyright (c) 2026 Tinkerbug Robotics
 //
 // MIT License
 //
@@ -6,6 +6,7 @@
 #pragma once
 #include <Arduino.h>
 #include <WebServer.h>
+#include <Preferences.h>
 #include <TR_SkyTraqNMEA.h>
 
 class TR_RTKRoverWebUI
@@ -17,7 +18,8 @@ public:
                      TR_SkyTraqNMEA::GnssNmeaData& latest,
                      bool& hasGnss,
                      unsigned long& lastRtcmMs,
-                     uint16_t& lastRtcmBlockBytes);
+                     uint16_t& lastRtcmBlockBytes,
+                     uint8_t& numBatteryCellsRuntime);
 
     // Register routes and start the HTTP server
     void begin();
@@ -32,12 +34,14 @@ private:
     bool& g_has_gnss_;
     unsigned long& g_lastRtcmMs_;
     uint16_t& g_lastRtcmBlockBytes_;
+    uint8_t& num_battery_cells_;
 
     // Internal helpers
     const char* rtkModeFromQuality(uint8_t q);
 
     void handleRoot();
     void handleGnssJson();
+    void handleSetCells();
     void handleNotFound();
     
 };
